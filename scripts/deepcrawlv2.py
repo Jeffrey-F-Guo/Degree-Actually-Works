bb07a3c5db8da24e0b82b69af49ad8ac0bea1e83
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2025-09-02 19:26:09 -0700

    Created more efficient deep crawling to scrape WIN events page

--- scripts/deepcrawlv2.py
+++ scripts/deepcrawlv2.py
@@ -2,6 +2,7 @@ import asyncio
 from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, LLMConfig
 from crawl4ai.deep_crawling import BFSDeepCrawlStrategy
 from crawl4ai.extraction_strategy import LLMExtractionStrategy
+from crawl4ai.deep_crawling.filters import FilterChain, DomainFilter
 from pydantic import BaseModel, Field
 from typing import List
 import os
@@ -47,13 +48,14 @@ async def main():
                     - Academic events, deadlines
                     """
     )
-
+    # link_filter = FilterChain([DomainFilter(allowed_domains=['win.wwu.edu'])])
     # Configure a 1-level deep crawl (depth=1 means one level deep)
     config = CrawlerRunConfig(
         deep_crawl_strategy=BFSDeepCrawlStrategy(
             max_depth=1,
             include_external=False,
             max_pages=10,
+            # filter_chain = link_filter,
             # link_filter_patterns=[
             #     r".*event.*",  # Include URLs containing 'event'
             #     r".*calendar.*",  # Include URLs containing 'calendar'
@@ -73,17 +75,28 @@ async def main():
 
             all_events = []
 
-            # Process each result
+            # Process each result -- go through the events for each page
             for i, result in enumerate(results):
                 print(f"\nPage {i+1}:")
                 print(f"URL: {result.url}")
                 print(f"Depth: {result.metadata.get('depth', 0)}")
                 print(f"Success: {result.success}")
 
-                for result in results:
-                    if result.success:
-                        extracted_events_model = result.extracted_content
-                        all_events.extend(extracted_events_model)
+                content = json.loads(result.extracted_content)
+                print("content", content)
+                events_list = content[0]["events"]
+                if events_list:
+                    extracted_events = ExtractedEvents(events=events_list)
+                    all_events.extend(extracted_events.events)
+                    # for event in events_list:
+                    #     print("PROGRESSS")
+                    #     print(event)
+                    #     extracted_event = EventEntry(**event)
+                    #     all_events.append(extracted_event)
+
+                else:
+                    print("Did not find events on page")
+            
 
 
 
@@ -92,6 +105,7 @@ async def main():
             print(f"SUMMARY: Found {len(all_events)} total events across all pages")
             print("="*60)
 
+            print(all_events)
             # Display all events
             if all_events:
                 for i, event in enumerate(all_events):
