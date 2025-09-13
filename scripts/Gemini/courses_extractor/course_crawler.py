3c8de55f4ba537d74f22fe39b3b2677e8a6cd2a5
Jeffrey <jeffrey.guo00703@gmail.com>
2025-09-13 00:14:18 -0700

    Course crawler only gets first course

--- scripts/Gemini/courses_extractor/course_crawler.py
+++ scripts/Gemini/courses_extractor/course_crawler.py
@@ -1,5 +1,5 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, BrowserConfig
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, BrowserConfig, JsonCssExtractionStrategy
 import os
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
@@ -13,24 +13,45 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-# base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10593" # cs
+base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10593" # cs
 
 #base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=11053" # business
 
-base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10724" # math
+# base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10724" # math
+# base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10754" # psych
 
-async def crawl(base_url: str) -> List:
-    b_config = BrowserConfig(headless=False)
+
+async def crawl(base_url: str, debug_mode: bool) -> List:
+    schema = {
+        "name": "class list",
+        "baseSelector": "body",
+        "fields": [
+            {
+                "name": "course_name",
+                "selector": "li.acalog-course span a",
+                "type": "text",
+            }
+
+        ]
+    }
+    extraction_strategy = JsonCssExtractionStrategy(schema=schema)
+    config = CrawlerRunConfig(extraction_strategy=extraction_strategy)
+    b_config = BrowserConfig(headless=(not debug_mode))
     async with AsyncWebCrawler(config=b_config) as crawler:
         # navigate once to the base URL
-        results = await crawler.arun(url=base_url)
-        with open("math_courses.html", "w") as f:
+        results = await crawler.arun(url=base_url, config=config)
+        await asyncio.sleep(2)
+        print(results.extracted_content)
+        if not results.extracted_content:
+            logger.warning("No courses found")
+            return []
+        # for result in results.extracted_content:
+        #     print(result)
+        with open("psych_courses.html", "w") as f:
             f.write(results.html)
-
-        print(results.markdown)
 
     return []
 
 
 if __name__ == "__main__":
-    asyncio.run(crawl(base_url))
+    asyncio.run(crawl(base_url, debug_mode=True))
