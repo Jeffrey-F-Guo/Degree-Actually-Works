2f3ccf83c61801d5bb4e00ffca4b4a49ccca7819
Jeffrey <jeffrey.guo00703@gmail.com>
2025-09-13 00:41:09 -0700

    Fixed course crawler issue

--- scripts/Gemini/courses_extractor/course_crawler.py
+++ scripts/Gemini/courses_extractor/course_crawler.py
@@ -8,14 +8,15 @@ from pydantic import BaseModel, Field
 from typing import List
 import logging
 import csv
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10593" # cs
+# base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10593" # cs
 
-#base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=11053" # business
+base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=11053" # business
 
 # base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10724" # math
 # base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10754" # psych
@@ -24,11 +25,11 @@ base_url = "https://catalog.wwu.edu/preview_program.php?catoid=22&poid=10593" # 
 async def crawl(base_url: str, debug_mode: bool) -> List:
     schema = {
         "name": "class list",
-        "baseSelector": "body",
+        "baseSelector": "li.acalog-course",
         "fields": [
             {
                 "name": "course_name",
-                "selector": "li.acalog-course span a",
+                "selector": "span a",
                 "type": "text",
             }
 
@@ -37,20 +38,22 @@ async def crawl(base_url: str, debug_mode: bool) -> List:
     extraction_strategy = JsonCssExtractionStrategy(schema=schema)
     config = CrawlerRunConfig(extraction_strategy=extraction_strategy)
     b_config = BrowserConfig(headless=(not debug_mode))
+    course_list = []
     async with AsyncWebCrawler(config=b_config) as crawler:
         # navigate once to the base URL
         results = await crawler.arun(url=base_url, config=config)
-        await asyncio.sleep(2)
         print(results.extracted_content)
         if not results.extracted_content:
             logger.warning("No courses found")
             return []
-        # for result in results.extracted_content:
-        #     print(result)
-        with open("psych_courses.html", "w") as f:
-            f.write(results.html)
+        courses = json.loads(results.extracted_content)
+        for course in courses:
+            if course:
+                course_name = course["course_name"].replace("\xa0 - ", "")
+                print(course_name)
+                course_list.append(course_name)
+    return course_list
 
-    return []
 
 
 if __name__ == "__main__":
